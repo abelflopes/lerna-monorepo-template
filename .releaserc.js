ba13e23bef7fb2f7fb76eac1b7d0080b481dcfbd
Abel Lopes <abelflopes@gmail.com>
2023-10-27 00:29:52 +0100

    chore: prevent main package from being published

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,5 @@
+process.env.SR_CONFIG_NPM_PUBLISH = "false"; // if set to false will not attempt to publish the main package to the registry
+
 /** @type import("semantic-release").GlobalConfig */
 const config = {
   extends: "semantic-release-config-conventional",
