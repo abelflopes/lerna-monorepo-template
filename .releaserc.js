f80735b11386740aed4aafd0c15fc34554b7eee2
Abel Lopes <abelflopes@gmail.com>
2023-10-27 01:18:46 +0100

    chore: use different changelog file from lerna

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,6 @@
 // https://www.npmjs.com/package/semantic-release-config-conventional
-process.env.SR_CONFIG_NPM_PUBLISH = "false"; // if set to false will not attempt to publish the main package to the registry
+process.env.SR_CONFIG_NPM_PUBLISH = "false"; // prevent the main package to the registry
+process.env.SR_CONFIG_CHANGELOG_FILE = "docs/PROJECT-CHANGELOG.md"; // use different changelog file from lerna
 
 /** @type import("semantic-release").GlobalConfig */
 const config = {
