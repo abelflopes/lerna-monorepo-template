9368326acf4168efdbc5a22308c781deb78890ee
Abel Lopes <abelflopes@gmail.com>
2023-10-28 01:47:38 +0100

    docs: move location of project changelog

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,5 @@
 // https://www.npmjs.com/package/semantic-release-config-conventional
 process.env.SR_CONFIG_NPM_PUBLISH = "false"; // prevent the main package to the registry
-process.env.SR_CONFIG_CHANGELOG_FILE = "docs/PROJECT-CHANGELOG.md"; // use different changelog file from lerna
 
 /** @type import("semantic-release").GlobalConfig */
 const config = {
